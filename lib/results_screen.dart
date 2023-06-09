4fe4558ce629280f1d3bab93b3a634d8c9ab7c8a
Jubayer Islam <jubayerislam@iut-dhaka.edu>
2023-06-09 22:11:34 +0600

    answer screen done , but not the scrollable and ui part

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -9,7 +11,12 @@ class ResultsScreen extends StatelessWidget {
     final List<Map<String, Object>> summary = [];
 
     for (var i = 0; i < chosenAnswers.length; i++) {
-      summary.add({});
+      summary.add({
+        'question_index': 1,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
     }
 
     return summary;
@@ -17,6 +24,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -24,10 +37,11 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
+             Text('You answered $numCorrectQuestions out of $numTotalQuestions correctly!',),
             const SizedBox(
               height: 30,
             ),
+            QuestionsSummary(summaryData),
             TextButton(
               onPressed: () {},
               child: const Text('Restart Quiz!'),
